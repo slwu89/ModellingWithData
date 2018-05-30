c26fa7c1c5fbfee7e65d668c444720f6658db0b5
slwu89 <slwu89@berkeley.edu>
2018-05-29 21:15:05 -0700

    sync

---
+++ src/paper.h
@@ -0,0 +1,11 @@
+/* ##################################################
+#   Examples from paper "A useful algebraic system of statistical models"
+#   Sean Wu
+################################################## */
+
+/* C headers */
+#include <apop.h>
+
+/* R's C API */
+#include <R.h>
+#include <Rinternals.h>
